fa17d54b1a3cad03954acc05ef39b30fc0e29d24
SOHYEON00 <ohsohyeon24@gmail.com>
2021-01-11 16:44:28 +0900

    #1.2 Auth, Router Set up - create simple Authentication Setup in ReactJS - use Router, Route, Switch

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBs2-bbGPfQjj4vsMPqBEaC1ooB4KB0yBg",
-    authDomain: "nwitter-9d504.firebaseapp.com",
-    projectId: "nwitter-9d504",
-    storageBucket: "nwitter-9d504.appspot.com",
-    messagingSenderId: "897374426800",
-    appId: "1:897374426800:web:e04e5e446af838c9a78d75"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
