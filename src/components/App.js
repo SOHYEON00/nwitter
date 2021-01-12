d73e851bca302726ea1ecc91d5c7efee18989263
SOHYEON00 <ohsohyeon24@gmail.com>
2021-01-13 00:57:45 +0900

    feat: firebase 사용해 로그인/계정생성 기능 구현 - newAccount state의 상태로 계정생성 또는 로그인 기능 실행   true: 계정생성 / false: 로그인
    - 계정 생성시, 자동 로그인 후 home으로 route
    
    - form 밑의 <span> 클릭시, 계정 생성 또는 로그인 선택 가능

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fBase";
 
 // App.js 가 모든 로직들을 다룰 예정
 function App() {
-  // authService.currentUser : 현재 로그인 한 유저
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  // authService.currentUser : 현재 로그인 한 유저를 가리키나, 실제로 firebase를 통해 로그인/로그아웃한 지는 알 수 없다.
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [init, setInit] = useState(false); // 초기화 상태
+
+  //onAuthStateChanged : 사용자의 로그인 상태를 관찰하는 관찰자를 추가해줌
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      user ? setIsLoggedIn(true) : setIsLoggedIn(false);
+      setInit(true);
+    });
+  }, []);
 
   return (
     <div className="App">
-      <AppRouter isLoggedIn={isLoggedIn} />
-      <footer>&copy; {new Date().getFullYear()} Nwitter - nomardcoders.com</footer>
+      {init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Initializing..."}
+      <footer>
+        &copy; {new Date().getFullYear()} Nwitter - nomardcoders.com
+      </footer>
     </div>
   );
 }
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,32 +1,69 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { authService } from "fBase";
 
 const Auth = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  //value를 사용하기 위해 state 사용
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true); //새로운 유저인지 true: 계정 생성, false: 로그인
+  const [error, setError] = useState("");
 
-    const onValueChanged = (event) => { 
-        const {
-            target: {name, value}
-        } = event; //get name and value from changed JSX dom
+  const onValueChanged = (event) => {
+    const {
+      target: { name, value }, //target: 변경이 일어난 부분
+    } = event; //get name and value
 
-        if(name === 'email'){
-            setEmail(value);
-        } else if(name === 'password') {
-            setPassword(value);
-        }
+    if (name === "email") {
+      setEmail(value); //input의 value가 변경될 때마다 state의 값도 변한다. -> 리렌더링
+    } else if (name === "password") {
+      setPassword(value);
     }
-
-    const onFormSubmit = (event) => {
-        event.preventDefault();
+  };
+// seetPersistance : 유저가 저장되는 방식 설정 local / session / none
+  const onFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const data = newAccount
+        ? await authService.createUserWithEmailAndPassword(email, password) // Create account
+        : await authService.signInWithEmailAndPassword(email, password); // Log in
+        console.log(data);
+    } catch (error) {
+      setError(error.message);
     }
+  };
+  
+  //newAccount를 이전값과 반대인 값으로 바꿔주는 함수. true <-> false
+  const toggleAccount = () => setNewAccount((prev) => !prev);
 
   return (
     <div>
       <form onSubmit={onFormSubmit}>
-        <input name="email" type="text" placeholder="Email" required onChange={onValueChanged}/>
-        <input name="password" type="password" placeholder="Password" required onChange={onValueChanged}/>
-        <input type="submit" />
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onValueChanged}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onValueChanged}
+        />
+        <input
+          type="submit"
+          value={newAccount ? "Create new account" : "Log in"}
+        />
       </form>
+      {/* 반대의 기능 사용 create new account <-> sign in */}
+      <span onClick={toggleAccount}> 
+        {newAccount ? "Sign In" : "Create new account"}
+      </span>
+      {error}
       <div>
         <button>Google 이메일로 로그인하기</button>
         <button>Github 이메일로 로그인하기</button>
