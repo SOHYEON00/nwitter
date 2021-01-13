7deeadd95bb7ddab2b1d553eb5a4afc5aba493ab
SOHYEON00 <ohsohyeon24@gmail.com>
2021-01-13 16:27:23 +0900

    feat: Read all nweet from db - firebase의 db에 저장된 collection, nweets에서 모든 요소(document)추출 - /HOME에 출력

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,29 +1,62 @@
 import { dbService } from "fBase";
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
-    const [nweet, setNweet] = useState("");
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
 
-    const onTextChange = (event) => {
-        const {target: {value}} = event;
-        setNweet(value);
-    };
-    const onSubmitHandler = async(event) => {
-        event.preventDefault();
-        await dbService.collection("nweets").add({
-            nweet,
-            createdAt: Date.now()
-        });
-        setNweet("");
-    } ;
+  const getNweets = async () => {
+    //get documents from 'nweets'collections
+    const dbNweets = await dbService.collection("nweets").get(); //get all info of 'nweet'collection
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]); //make an array of documents.datas
+    });
+  };
 
-    return (
-        <div>
-            <form onSubmit={onSubmitHandler}>
-                <input type="text" value={nweet} onChange={onTextChange} placeholder="What's on your mind?"/>
-                <input type="submit" value="Nweet"/> 
-            </form>
-        </div>
-    )
+  useEffect(() => {
+    getNweets();
+  }, []);
+
+  const onTextChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    await dbService.collection("nweets").add({
+      nweet,
+      createdAt: Date.now(),
+    });
+    setNweet("");
+  };
+  console.log(nweets);
+  return (
+    <div>
+      <form onSubmit={onSubmitHandler}>
+        <input
+          type="text"
+          value={nweet}
+          onChange={onTextChange}
+          placeholder="What's on your mind?"
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+      <div>
+        {nweets.map((nweet) => {
+          return (
+            <div key={nweet.id}>
+              <h4>{nweet.nweet}</h4>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default Home;
