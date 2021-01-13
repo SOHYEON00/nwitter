bb5f7a6f72be12e038c8445f88a8e4dca657ab36
SOHYEON00 <ohsohyeon24@gmail.com>
2021-01-13 14:48:04 +0900

    feat: create a nweet(tweet message) - firebase.firestore를 사용함 - nweets라는 collection 생성   새로운 메시지를 제출할 때마다 collection의 document 생성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,29 @@
-import React from "react";
+import { dbService } from "fBase";
+import React, {useState} from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [nweet, setNweet] = useState("");
+
+    const onTextChange = (event) => {
+        const {target: {value}} = event;
+        setNweet(value);
+    };
+    const onSubmitHandler = async(event) => {
+        event.preventDefault();
+        await dbService.collection("nweets").add({
+            nweet,
+            createdAt: Date.now()
+        });
+        setNweet("");
+    } ;
+
+    return (
+        <div>
+            <form onSubmit={onSubmitHandler}>
+                <input type="text" value={nweet} onChange={onTextChange} placeholder="What's on your mind?"/>
+                <input type="submit" value="Nweet"/> 
+            </form>
+        </div>
+    )
+};
 export default Home;
