c7e89b179d1169497d9054d574ae904a93694556
SOHYEON00 <ohsohyeon24@gmail.com>
2021-01-13 10:37:33 +0900

    feat: Google/Github으로 로그인 기능 추가 - firebase의 provider를 사용

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { authService } from "fBase";
+import { firebaseInstance, authService } from "fBase";
 
 const Auth = () => {
   //value를 사용하기 위해 state 사용
@@ -35,6 +35,19 @@ const Auth = () => {
   //newAccount를 이전값과 반대인 값으로 바꿔주는 함수. true <-> false
   const toggleAccount = () => setNewAccount((prev) => !prev);
 
+  const onSocialClick = async(event) => {
+    const {target: {name}} = event;
+    let provider;
+    
+    if(name === 'google'){
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if(name === 'github'){
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
+
   return (
     <div>
       <form onSubmit={onFormSubmit}>
@@ -65,8 +78,8 @@ const Auth = () => {
       </span>
       {error}
       <div>
-        <button>Google 이메일로 로그인하기</button>
-        <button>Github 이메일로 로그인하기</button>
+        <button onClick={onSocialClick} name="google">Google 이메일로 로그인하기</button>
+        <button onClick={onSocialClick} name="github">Github 이메일로 로그인하기</button>
       </div>
     </div>
   );
