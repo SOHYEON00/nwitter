f82cc6abe6431dd086603718c8a4a9eadf81f802
SOHYEON00 <ohsohyeon24@gmail.com>
2021-01-15 11:38:35 +0900

    feat: Nweet 삭제/수정 기능 추가 - 수정 : doc.update() 사용 - 삭제 : doc.delete() 사용
    Nweet 컴포넌트에서 진행됨

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,13 +1,63 @@
-import React from "react";
+import { dbService } from "fBase";
+import React, { useState } from "react";
 
 function Nweet({ nweetObj, isOwner }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.text);
+
+  const onDeleteClick = async () => {
+    const ok = window.confirm("정말 삭제하시겠습니까?");
+    if (ok) {
+      await dbService.doc(`nweets/${nweetObj.id}`).delete(); //id를 알고 있기 때문에 가능./
+    }
+  };
+
+  const toggleEditing = () => {
+    setIsEditing((prev) => !prev);
+  };
+
+  const onTextChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewNweet(value);
+  };
+
+  const onSubmitHandler = async(e) => {
+    e.preventDefault();
+    await dbService.doc(`nweets/${nweetObj.id}`).update({
+        text: newNweet,
+    });
+    setIsEditing(false);
+  };
+
+
   return (
     <div>
-      <h4>{nweetObj.text}</h4>
-      {isOwner && (
+      {isEditing ? (
         <>
-          <button>Delete</button>
-          <button>Edit</button>
+          <form onSubmit={onSubmitHandler}>
+            <input
+              type="text"
+              onChange={onTextChange}
+              required
+              placeholder="당신의 Nweet을 수정하세요."
+              value={newNweet}
+            />
+            <input type="submit" value="Nwee 수정"/>
+          </form>
+          <button onClick={toggleEditing}>취소</button>
+        </>
+      ) : (
+        <>
+          {" "}
+          <h4>{nweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete</button>
+              <button onClick={toggleEditing}>Edit</button>
+            </>
+          )}
         </>
       )}
     </div>
