b790d5fdb1204c966c87e820a6a927abc0f74c7c
SOHYEON00 <ohsohyeon24@gmail.com>
2021-01-15 10:48:58 +0900

    feat: nweet Realtime으로 변경하기 - onSnapshot() 리스너를 이용 - 기존, HOME에서 사용한 getNweets()함수 대신 사용 - 덕분에 RealTime으로 db 정보를 가져올 수 있음

--- src/components/App.js
+++ src/components/App.js
@@ -7,18 +7,28 @@ function App() {
   // authService.currentUser : 현재 로그인 한 유저를 가리키나, 실제로 firebase를 통해 로그인/로그아웃한 지는 알 수 없다.
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [init, setInit] = useState(false); // 초기화 상태
+  const [userObj, setUserObj] = useState(null); //유저 정보를 가지는 상태
 
   //onAuthStateChanged : 사용자의 로그인 상태를 관찰하는 관찰자를 추가해줌
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
-      user ? setIsLoggedIn(true) : setIsLoggedIn(false);
+      if (user) {
+        setIsLoggedIn(true);
+        setUserObj(user); //로그인한 유저 정보를 저장
+      } else {
+        setIsLoggedIn(false);
+      }
       setInit(true);
     });
   }, []);
 
   return (
     <div className="App">
-      {init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Initializing..."}
+      {init ? (
+        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+      ) : (
+        "Initializing..."
+      )}
       <footer>
         &copy; {new Date().getFullYear()} Nwitter - nomardcoders.com
       </footer>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,24 +1,32 @@
 import { dbService } from "fBase";
 import React, { useState, useEffect } from "react";
 
-const Home = () => {
+const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
 
-  const getNweets = async () => {
-    //get documents from 'nweets'collections
-    const dbNweets = await dbService.collection("nweets").get(); //get all info of 'nweet'collection
-    dbNweets.forEach((document) => {
-      const nweetObject = {
-        ...document.data(),
-        id: document.id,
-      };
-      setNweets((prev) => [nweetObject, ...prev]); //make an array of documents.datas
-    });
-  };
+  // const getNweets = async () => {
+  //   //get documents from 'nweets'collections
+  //   const dbNweets = await dbService.collection("nweets").get(); //get all info of 'nweet'collection
+  //   dbNweets.forEach((document) => {
+  //     const nweetObject = {
+  //       ...document.data(),
+  //       id: document.id,
+  //     };
+  //     setNweets((prev) => [nweetObject, ...prev]); //make an array of documents.datas
+  //   });
+    
+  // };
 
   useEffect(() => {
-    getNweets();
+    //onShapshot 메소드는 listener, DB의 변화를 바로바로 알려줌.
+    dbService.collection("nweets").onSnapshot(snapshot => {
+      const nweetArray = snapshot.docs.map(doc => ({ // getNweets()대신 snapShot사용, realtime으로 db 정보 가져올 수 있음
+        id: doc.id,
+        ...doc.data()
+      }));
+      setNweets(nweetArray);
+    });
   }, []);
 
   const onTextChange = (event) => {
@@ -30,12 +38,13 @@ const Home = () => {
   const onSubmitHandler = async (event) => {
     event.preventDefault();
     await dbService.collection("nweets").add({
-      nweet,
+      text: nweet,
       createdAt: Date.now(),
+      creator: userObj.uid //props로 받은 유저정보 중 id
     });
     setNweet("");
   };
-  console.log(nweets);
+
   return (
     <div>
       <form onSubmit={onSubmitHandler}>
@@ -51,7 +60,7 @@ const Home = () => {
         {nweets.map((nweet) => {
           return (
             <div key={nweet.id}>
-              <h4>{nweet.nweet}</h4>
+              <h4>{nweet.text}</h4>
             </div>
           );
         })}
