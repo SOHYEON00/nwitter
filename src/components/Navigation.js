d860570131e4177fd3792ef51ffa059b21432b5a
SOHYEON00 <ohsohyeon24@gmail.com>
2021-01-22 01:45:45 +0900

    feat: nweet 게시할 때, like 프로퍼티 db에 추가
    - nweet를 게시하는 기능을 가진 nweetFactory 컴포넌트에 작성

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,21 +3,39 @@ import {Link} from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
+import {useLocation} from "react-router-dom";
 
 function Navigation({ userObj }) {
+  const location = useLocation();
 
+  const showCurrentPage = () => {
+    const pathHome = document.getElementById("pathHome");
+    const pathProfile = document.getElementById("pathProfile");
+ 
+    if(location.pathname === "/"){
+      pathHome.classList.add("currentPage");
+      pathProfile.classList.remove("currentPage")
+    } else if(location.pathname === "/profile"){
+      pathHome.classList.remove("currentPage");
+      pathProfile.classList.add("currentPage");
+    }
+  }
+
+  showCurrentPage();
+  
     return (
       <nav>
         <ul className="navMenu">
           <li>
-            <Link to="/" className="home_link">
+            <Link to="/" className="navLink" >
               <FontAwesomeIcon icon={faTwitter} color={"#04AAFF"} size="2x" />
-            </Link>
+              <span className="navBar" id="pathHome">HOME</span>
+            </Link> 
           </li>
           <li>
-            <Link to="/profile" className="profile_link">
+            <Link to="/profile" className="navLink" >
               <FontAwesomeIcon icon={faUser} color={"#04AAFF"} size="2x" />
-              <span>
+              <span className="navBar" id="pathProfile">
                 {userObj.displayName
                   ? `${userObj.displayName}의 Profile`
                   : "Profile"}
